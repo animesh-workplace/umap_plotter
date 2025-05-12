a1660530fe0ee369f0198ac0d24a3c594004fa23
Animesh <animesh.workplace@gmail.com>
2025-05-12 13:19:33 +0530

    updated file - tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,56 @@
-/** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate";
+import { setupInspiraUI } from "@inspira-ui/plugins";
 
-const defaultTheme = require('tailwindcss/defaultTheme')
-const fontFamily = defaultTheme.fontFamily
-fontFamily['sans'] = ['Averta', 'system-ui']
+export default {
+  darkMode: "selector",
+  safelist: ["dark"],
+  prefix: "",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
 
-module.exports = {
-  theme: {},
-  darkMode: ['class'],
-  content: [
-    './pages/**/*.vue',
-    './layouts/**/*.vue',
-    './nuxt.config.{js,ts}',
-    './plugins/**/*.{js,ts}',
-    './components/**/*.{js,vue,ts}',
-  ],
-}
+  plugins: [animate, setupInspiraUI],
+};
