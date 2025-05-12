6bb47f13dda6ec09e9ac5d0ff07e64b3f53122cc
Animesh <animesh.workplace@gmail.com>
2025-05-12 13:19:33 +0530

    added file - pages/test.vue

---
+++ pages/test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div data-v-9a9d5efb=""
+        class="relative flex h-screen w-screen bg-[#fffefe] flex-col items-center justify-center overflow-hidden rounded-lg border-parent dark:border-none bg-background c-#282f38 overflow-x-scroll dark:bg-[#000000] flex-wrap">
+        <svg data-v-9a9d5efb="" aria-hidden="true"
+            class="pointer-events-none fill-neutral-400/80 absolute inset-0 size-full" width="16" height="16" x="0"
+            y="0" cx="1" cy="1" cr="1">
+            <defs>
+                <pattern id="pattern-v-84" width="16" height="16" patternUnits="userSpaceOnUse"
+                    patternContentUnits="userSpaceOnUse" x="0" y="0">
+                    <circle id="pattern-circle" cx="1" cy="1" r="1"></circle>
+                </pattern>
+            </defs>
+            <rect width="100%" height="100%" strokeWidth="0" fill="url(#pattern-v-84)"></rect>
+        </svg>
+    </div>
+</template>
+
+<script setup>
+</script>
