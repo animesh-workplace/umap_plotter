96cd921419409562b6d5939dc8d48ffec193c757
Animesh <animesh.workplace@gmail.com>
2025-05-13 13:17:21 +0530

    updated file - pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-    <h1 class="text-2xl text-center mb-2">UMAP Visualization</h1>
-    <div class="flex justify-center z-10">
-      <GeneSearch />
+  <div class="p-8 grid grid-cols-1 justify-items-center">
+    <div class="mb-4 text-center">
+      <h2 class="text-xl font-semibold mb-2">
+        Gene Expression UMAP Visualization
+      </h2>
+      <p class="text-gray-500">Select a gene to visualize expression levels</p>
     </div>
-    <UMAPPlot :data="umapData" class="-mt-8" />
+
+    <div class="grid grid-cols-4 gap-10 min-w-full">
+      <UMAPGraph />
+      <UMAPGraph />
+      <UMAPGraph />
+      <UMAPGraph />
+    </div>
   </div>
 </template>
 
-<script setup>
-import { useUmapDataStore } from '@/stores/umapData'
-
-const store = useUmapDataStore()
-const umapData = computed(() => store.umapPoints)
-</script>
+<script setup></script>
