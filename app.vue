6d0fec19f2a3aa00b69b8f82fc02b698022332d0
Animesh <animesh.workplace@gmail.com>
2025-05-13 12:19:39 +0530

    updated file - app.vue

--- app.vue
+++ app.vue
@@ -8,14 +8,14 @@
 </template>
 
 <script setup>
-import { useGeneOptionsStore } from '@/stores/geneInfo'
-import { useUmapDataStore } from '@/stores/umapData'
+// import { useGeneOptionsStore } from '@/stores/geneInfo'
+// import { useUmapDataStore } from "@/stores/umapData";
 
-const umapStore = useUmapDataStore()
-const store = useGeneOptionsStore()
+// const umapStore = useUmapDataStore();
+// const store = useGeneOptionsStore()
 
-await Promise.all([
-  store.fetchGenes(),
-  umapStore.fetchUmapData()
-])
+// await Promise.all([
+// store.fetchGenes(),
+//   umapStore.fetchUmapData(),
+// ]);
 </script>
