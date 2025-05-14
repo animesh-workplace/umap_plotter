7090ac52e25b1a075b7a3b8ce561c98e48fa3d57
Animesh <animesh.workplace@gmail.com>
2025-05-14 19:46:09 +0530

    updated file - nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,42 @@
-import Aura from "@primeuix/themes/aura";
+import Aura from '@primeuix/themes/aura'
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  css: ["@/assets/css/main.css"],
-  devtools: { enabled: true },
-  icon: { clientBundle: { scan: true }, serverBundle:{collections: ['akar-icons']} },
-  primevue: {
-    options: {
-      ripple: true,
-      theme: { preset: Aura, options: { darkModeSelector: ".dark" } },
-    },
-  },
-  modules: [
-    "@nuxt/icon",
-    "@nuxt/fonts",
-    "@pinia/nuxt",
-    "nuxt-echarts",
-    "@nuxt/eslint",
-    "@vueuse/nuxt",
-    // 'reka-ui/nuxt',
-    "motion-v/nuxt",
-    "@nuxtjs/tailwindcss",
-    "@primevue/nuxt-module",
-  ],
-  echarts: {
-    ssr: true,
-    renderer: ["canvas"],
-    charts: ["ScatterChart"],
-    features: ["UniversalTransition"],
-    components: [
-      "GridComponent",
-      "TitleComponent",
-      "DatasetComponent",
-      "TooltipComponent",
-      "ToolboxComponent",
-      "VisualMapComponent",
-    ],
-  },
-});
+	compatibilityDate: '2024-11-01',
+	css: ['@/assets/css/main.css'],
+	devtools: { enabled: true },
+	icon: { clientBundle: { scan: true }, serverBundle: { collections: ['akar-icons'] } },
+	primevue: {
+		options: {
+			ripple: true,
+			theme: { preset: Aura, options: { darkModeSelector: '.dark' } },
+		},
+		components: { include: ['Skeleton', 'Button', 'ToggleSwitch', 'Autocomplete'] },
+	},
+	build: { transpile: ['echarts', 'echarts-gl', 'zrender'] },
+	modules: [
+		'@nuxt/icon',
+		'@nuxt/fonts',
+		'@pinia/nuxt',
+		'nuxt-echarts',
+		'@nuxt/eslint',
+		'@vueuse/nuxt',
+		// 'reka-ui/nuxt',
+		'motion-v/nuxt',
+		'@nuxtjs/tailwindcss',
+		'@primevue/nuxt-module',
+	],
+	echarts: {
+		ssr: true,
+		renderer: ['canvas'],
+		charts: ['ScatterChart'],
+		features: ['UniversalTransition'],
+		components: [
+			'GridComponent',
+			'TitleComponent',
+			'DatasetComponent',
+			'TooltipComponent',
+			'ToolboxComponent',
+			'VisualMapComponent',
+		],
+	},
+})
