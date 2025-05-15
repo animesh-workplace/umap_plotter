4c9be51822e39e1dde92a63b54efe34644ccb3ab
Animesh <animesh.workplace@gmail.com>
2025-05-15 10:41:22 +0530

    updated file - pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-	<div>
-		<GraphUMAP2D />
+	<div class="p-4 grid grid-cols-1 justify-items-center">
+		<div class="mb-2 text-center">
+			<h2 class="text-xl font-semibold mb-1">Gene Expression UMAP Visualization</h2>
+			<p class="text-gray-500">Select a gene to visualize expression levels</p>
+		</div>
+		<div class="grid grid-cols-6 min-w-full">
+			<GraphManager class="col-span-2 col-start-3" />
+		</div>
 	</div>
 </template>
 
