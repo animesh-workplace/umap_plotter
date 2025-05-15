bf03ebdaceb33e7727d88d5318f2969d7cf4bfa5
Animesh <animesh.workplace@gmail.com>
2025-05-15 15:46:35 +0530

    updated file - nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,24 @@ export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	css: ['@/assets/css/main.css'],
 	devtools: { enabled: true },
-	icon: { clientBundle: { scan: true }, serverBundle: { collections: ['akar-icons'] } },
+	icon: { clientBundle: { scan: true }, serverBundle: { collections: ['akar-icons', 'tabler'] } },
 	primevue: {
 		options: {
 			ripple: true,
 			theme: { preset: Aura, options: { darkModeSelector: '.dark' } },
 		},
-		components: { include: ['Skeleton', 'Button', 'ToggleSwitch', 'Autocomplete', 'Select'] },
+		components: {
+			include: [
+				'Select',
+				'Button',
+				'Carousel',
+				'Skeleton',
+				'FloatLabel',
+				'ToggleSwitch',
+				'Autocomplete',
+				'SelectButton',
+			],
+		},
 	},
 	build: { transpile: ['echarts', 'echarts-gl', 'zrender'] },
 	modules: [
