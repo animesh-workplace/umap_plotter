97fc085492629e3dec2c8d623219878031b9df39
Animesh <animesh.workplace@gmail.com>
2025-05-15 10:41:22 +0530

    updated file - components/Graph/UMAP2D.vue

--- components/Graph/UMAP2D.vue
+++ components/Graph/UMAP2D.vue
@@ -1,64 +1,100 @@
 <template>
-	<div class="p-4 grid grid-cols-1 justify-items-center">
-		<div class="mb-2 text-center">
-			<h2 class="text-xl font-semibold mb-1">Gene Expression UMAP Visualization</h2>
-			<p class="text-gray-500">Select a gene to visualize expression levels</p>
-		</div>
-
-		<div class="grid grid-cols-6 gap-10 min-w-full">
-			<UMAP2DGraph ref="UMAPGraph" class="col-start-3 col-span-2" :scatterData="umapEmbedding" />
-		</div>
+	<div class="w-full mt-3 flex justify-center">
+		<VChart ref="chart" :option="chartOption" class="h-[45rem]" />
 	</div>
 </template>
 
 <script setup>
-import { motion } from 'motion-v'
-import { useGeneAPI } from '@/api/geneAPI'
-
 const props = defineProps({
 	colorScheme: { type: String, default: '#5470c6' },
+	colorByCluster: { type: Boolean, default: false },
+	scatterData: { type: Array, required: true, default: () => [] },
 })
 
-const UMAPGraph = ref(null)
+// Add a watcher to log the data when it changes
+watch(
+	() => props.scatterData,
+	(newValue) => {
+		updateChart()
+	},
+	{ deep: true },
+)
 
-const isLoading = ref(true)
-const umapEmbedding = ref([])
-const originalUmapEmbedding = ref(null)
+watch(
+	() => props.colorByCluster,
+	(newValue, oldValue) => {
+		updateChart()
+	},
+)
 
-const get2DEmbedding = async () => {
-	const { get2DUmapEmbedding } = useGeneAPI()
-	try {
-		const response = await get2DUmapEmbedding()
-
-		// Check if response exists and is an array before calling map
-		if (response && Array.isArray(response)) {
-			originalUmapEmbedding.value = response.map((item) => [item.x, item.y, item.cell_id, item.cluster, 0])
-
-			umapEmbedding.value = [...originalUmapEmbedding.value]
-			// // Update chart with the embedding data
-			// updateChart()
-		} else {
-			console.error('Invalid UMAP embedding response:', response)
-			umapEmbedding.value = []
-		}
-	} catch (err) {
-		console.error('Error fetching UMAP embedding:', err)
-		umapEmbedding.value = []
-	} finally {
-		isLoading.value = false
-	}
+const updateChart = () => {
+	chartOption.value.series[0].data = [...props.scatterData]
 }
-onMounted(() => {
-	nextTick(async () => {
-		try {
-			await get2DEmbedding()
-		} catch (err) {
-			console.error('Error initializing component:', err)
-		} finally {
-			isLoading.value = false
-			UMAPGraph.value.updateChart()
-		}
-	})
+
+const chartOption = ref({
+	animation: true,
+	yAxis: {
+		type: 'value',
+		axisLabel: { fontFamily: 'Averta', fontWeight: 500 },
+	},
+	xAxis: {
+		type: 'value',
+		axisLabel: { fontFamily: 'Averta', fontWeight: 500 },
+	},
+	tooltip: {
+		trigger: 'item',
+		axisPointer: { type: 'cross' },
+		textStyle: { fontFamily: 'Averta', fontWeight: 500 },
+		formatter: function (params) {
+			if (!params) {
+				return ''
+			}
+			const expressionText = params.value[4] !== 0 ? `Expression: ${params.value[4].toFixed(2)}` : ''
+			return (
+				`X: ${params.value[0].toFixed(2)}<br/>` +
+				`Y: ${params.value[1].toFixed(2)}<br/>` +
+				`Cell: ${params.value[2]}<br/>` +
+				`${expressionText}`
+			)
+		},
+		borderColor: '#fff',
+	},
+	grid: {
+		top: '0%',
+		left: '0%',
+		right: '0%',
+		bottom: '0%',
+		containLabel: true,
+	},
+	series: [
+		{
+			symbolSize: 4,
+			type: 'scatter',
+			data: props.scatterData,
+			itemStyle: {
+				opacity: 0.8,
+				color: (params) => {
+					const groupColors = {
+						'CAF-1': '#F08080',
+						'CAF-2': '#32CD32',
+						'CAF-3': '#A0522D',
+						'CAF-4': '#4682B4',
+						'CAF-5': '#556B2F',
+						'CAF-6': '#708090',
+						'CAF-7': '#CC5500',
+						'CAF-8': '#008080',
+						'CAF-9': '#9B1B30',
+					}
+					const cluster_id = params.data[3]
+					return props.colorByCluster ? groupColors[cluster_id] : props.colorScheme
+				},
+			},
+		},
+	],
+})
+
+defineExpose({
+	updateChart,
 })
 </script>
 
