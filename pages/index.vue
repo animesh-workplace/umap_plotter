dbcfc1e62f67c1fe693d26d88d0a2f54941924e9
Animesh <animesh.workplace@gmail.com>
2025-05-17 01:25:21 +0530

    updated file - pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="p-4 grid grid-cols-1 justify-items-center">
 		<div class="mb-2 text-center">
-			<h2 class="text-xl font-semibold mb-1">Gene Expression UMAP Visualization</h2>
+			<h2 class="text-xl font-semibold mb-1">Gene Expression Visualization</h2>
 		</div>
-		<div class="grid gap-4 lg:grid-cols-6 md:grid-cols-2 min-w-full mt-2">
+		<!-- <div class="grid gap-4 lg:grid-cols-6 md:grid-cols-2 min-w-full mt-2">
 			<Skeleton height="3rem" v-if="isLoading" class="col-span-2 col-start-3" />
 			<SelectButton
 				v-else
@@ -11,10 +11,10 @@
 				:options="options"
 				class="justify-center col-span-2 col-start-3"
 			/>
-		</div>
-		<div class="grid gap-4 lg:grid-cols-6 md:grid-cols-2 min-w-full mt-2">
-			<GraphManager class="col-span-2 col-start-3" />
-			<!-- <GraphManager class="col-span-2" /> -->
+		</div> -->
+		<div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 min-w-full mt-2">
+			<GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-start-2 xl:col-span-2" />
+			<GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2" />
 		</div>
 	</div>
 </template>
