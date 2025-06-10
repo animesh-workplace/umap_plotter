d037ff3e2ca826239449cc30c177c86ce935cb81
Animesh <animesh.workplace@gmail.com>
2025-06-10 15:55:39 +0530

    updated file - app.vue

--- app.vue
+++ app.vue
@@ -1,21 +1,10 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-    <NuxtRouteAnnouncer />
-  </div>
+	<div>
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+		<NuxtRouteAnnouncer />
+	</div>
 </template>
 
-<script setup>
-// import { useGeneOptionsStore } from '@/stores/geneInfo'
-// import { useUmapDataStore } from "@/stores/umapData";
-
-// const umapStore = useUmapDataStore();
-// const store = useGeneOptionsStore()
-
-// await Promise.all([
-// store.fetchGenes(),
-//   umapStore.fetchUmapData(),
-// ]);
-</script>
+<script setup></script>
