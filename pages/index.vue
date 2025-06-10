5f5a2c778595b54cc9aef6db9e2e82fc984ff342
Animesh <animesh.workplace@gmail.com>
2025-06-10 13:19:38 +0530

    updated file - pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,18 @@
 <template>
-	<div class="p-4 grid grid-cols-1 justify-items-center">
+	<div class="p-4 grid grid-cols-1 justify-items-center z-50 bg-white">
 		<div class="mb-2 text-center">
 			<h2 class="text-xl font-semibold mb-1">Gene Expression Visualization</h2>
 		</div>
-		<!-- <div class="grid gap-4 lg:grid-cols-6 md:grid-cols-2 min-w-full mt-2">
-			<Skeleton height="3rem" v-if="isLoading" class="col-span-2 col-start-3" />
-			<SelectButton
-				v-else
-				v-model="selection"
-				:options="options"
-				class="justify-center col-span-2 col-start-3"
-			/>
-		</div> -->
 		<div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 min-w-full mt-2">
-			<GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-start-2 xl:col-span-2" />
-			<GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2" />
+			<GraphManager
+				class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-start-2 xl:col-span-2 backdrop-blur rounded-lg"
+			/>
+			<GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg" />
 		</div>
 	</div>
 </template>
 
-<script setup>
-const isLoading = ref(true)
-const selection = ref('UMAP')
-const options = ref(['UMAP', 't-SNE'])
-
-onBeforeMount(() => {
-	nextTick(() => {
-		isLoading.value = false
-	})
-})
-</script>
+<script setup></script>
 
 <!-- Blue Color #0284c7 -->
 <!-- Red Color #9d174d -->
