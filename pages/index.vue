7c9ad4580c80076934c046255971b2e72e5fb664
Animesh <animesh.workplace@gmail.com>
2025-06-10 15:02:51 +0530

    updated file - pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,9 @@
 		</div>
 		<div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 min-w-full mt-2">
 			<GraphManager
-				class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-start-2 xl:col-span-2 backdrop-blur rounded-lg"
+				class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-start-3 xl:col-span-2 backdrop-blur rounded-lg"
 			/>
-			<GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg" />
+			<!-- <GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg" /> -->
 		</div>
 	</div>
 </template>
