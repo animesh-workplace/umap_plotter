36cf3f150cacf68a1f93e236802e8c091ad006d1
Animesh <animesh.workplace@gmail.com>
2025-06-12 23:47:51 +0530

    updated file - pages/test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,7 @@
 <template>
-	<div class="p-4 grid grid-cols-1 justify-items-center"></div>
+	<div class="p-4 grid grid-cols-4 justify-items-center">
+		<GraphHeatMap />
+	</div>
 </template>
 
 <script setup></script>
