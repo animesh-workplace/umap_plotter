2bf444af5dc12e4c2a673d1c75c448acca67e717
Animesh <animesh.workplace@gmail.com>
2025-06-13 10:35:49 +0530

    updated file - pages/gene_coexpression.vue

--- pages/gene_coexpression.vue
+++ pages/gene_coexpression.vue
@@ -4,16 +4,32 @@
 			<h2 class="text-xl font-semibold mb-1">Gene Co-expression Visualization</h2>
 		</div>
 		<div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 min-w-full mt-2">
-			<GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg" />
-			<GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg" />
-			<GraphManager class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg" />
+			<GraphManager
+				class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg"
+				colorScheme="#0284c7"
+			/>
+			<GraphManager
+				class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg"
+				colorScheme="#9d174d"
+			/>
+			<GraphManager
+				class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg"
+				colorScheme="#ca8a04"
+			/>
+			<GraphHeatMap class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 backdrop-blur rounded-lg" />
 		</div>
-		<!-- <GraphHeatMap
-			class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-span-2 xl:col-start-2 backdrop-blur rounded-lg"
-		/> -->
 	</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useGeneralDataStore } from '@/stores/generalData'
+
+onMounted(() => {
+	nextTick(() => {
+		const generalDataStore = useGeneralDataStore()
+		generalDataStore.updateNavBarPosition('Gene Co-expression')
+	})
+})
+</script>
 
 <style></style>
