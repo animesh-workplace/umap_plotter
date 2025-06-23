618b33a763f3f139d6d45e752c68505f5c0774cc
Animesh <animesh.workplace@gmail.com>
2025-06-23 15:16:14 +0530

    updated file - components/Graph/SpatialScatter.vue

--- components/Graph/SpatialScatter.vue
+++ components/Graph/SpatialScatter.vue
@@ -13,11 +13,13 @@
 
 <script setup>
 const validMin = ref(0)
-const validMax = ref(0)
+const validMax = ref(1)
 const chartRef = ref(null)
-const isLoading = ref(false)
+const isLoading = ref(true)
 const originalImageWidth = 778
 const originalImageHeight = 768
+const currentImageWidth = ref(0)
+const currentImageHeight = ref(0)
 
 const props = defineProps({
 	imageURL: { type: String, default: '' },
@@ -25,10 +27,86 @@ const props = defineProps({
 	scatterData: { type: Array, required: true, default: () => [] },
 })
 
+// watch(
+// 	() => props.scatterData,
+// 	(newValue) => {
+// 		updateChart()
+// 	},
+// 	{ deep: true },
+// )
+
+watch(
+	() => props.imageURL,
+	(newValue) => {
+		updateChart()
+	},
+	{ deep: true },
+)
+
+const getExpressionRange = () => {
+	const values = props.scatterData.map((item) => item[5])
+	return [Math.min(...values), Math.ceil(Math.max(...values))]
+}
+
+const calculateDimensions = () => {
+	if (!chartRef.value) return
+
+	const chartContainer = chartRef.value.$el
+	currentImageWidth.value = chartContainer.clientWidth
+	currentImageHeight.value = chartContainer.clientHeight
+}
+
+const updateChart = () => {
+	// Calculate expression range for visualMap
+	const [min, max] = getExpressionRange()
+	// Calculate the dimensions of the image
+	calculateDimensions()
+	// Make sure min and max are valid numbers and not the same value
+	const validMin = isFinite(min) ? min : 0
+	const validMax = isFinite(max) && max > validMin ? max : validMin + 1
+
+	chartOption.value = {
+		...chartOption.value,
+		visualMap: {
+			show: true,
+			right: 5,
+			dimension: 5,
+			top: 'center',
+			min: validMin,
+			max: validMax,
+			hoverLink: false,
+			calculable: true,
+			type: 'continuous',
+			orient: 'vertical',
+			inRange: {
+				color: [
+					'#000004',
+					'#1B0C41',
+					'#4F116F',
+					'#812581',
+					'#B5367A',
+					'#E55063',
+					'#FB8861',
+					'#FEC287',
+					'#F6F1B5',
+					'#FCFDBF',
+				],
+			},
+			textStyle: { fontFamily: 'Averta', fontWeight: 500 },
+		},
+	}
+	chartOption.value.series[0].data = [...props.scatterData]
+	chartOption.value.graphic.style = {
+		image: props.imageURL,
+		width: currentImageWidth.value,
+		height: currentImageHeight.value,
+	}
+}
+
 const chartOption = ref({
 	visualMap: {
-		right: 0,
-		show: false,
+		right: 5,
+		show: true,
 		dimension: 2,
 		top: 'center',
 		min: validMin,
@@ -66,8 +144,6 @@ const chartOption = ref({
 		inverse: false,
 		splitNumber: 40,
 		max: originalImageWidth * 0.77,
-		axisLabel: { lineStyle: { color: 'red' } },
-		splitLine: { lineStyle: { color: 'red' } },
 	},
 	yAxis: {
 		min: 0,
@@ -76,7 +152,6 @@ const chartOption = ref({
 		inverse: true, // Top-down coordinate system
 		splitNumber: 40,
 		max: originalImageHeight * 0.795,
-		splitLine: { lineStyle: { color: 'red' } },
 	},
 	graphic: {
 		z: -10,
@@ -84,7 +159,7 @@ const chartOption = ref({
 		left: 0,
 		type: 'image',
 		style: {
-			image: '',
+			image: props.imageURL,
 			width: originalImageWidth,
 			height: originalImageHeight,
 		},
@@ -95,12 +170,25 @@ const chartOption = ref({
 	},
 	series: [
 		{
-			data: [], // Add points here
 			symbolSize: 8,
 			type: 'scatter',
+			data: props.scatterData,
 			itemStyle: { color: 'green' },
 		},
 	],
+})
+
+onMounted(() => {
+	nextTick(() => {
+		try {
+			isLoading.value = false
+			setTimeout(() => {
+				updateChart()
+			}, 50)
+		} catch (err) {
+			console.log(err)
+		}
+	})
 })
 </script>
 
