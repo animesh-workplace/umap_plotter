52877fc3a417a68dcc91b5179f7a7004c82af7c5
Animesh <animesh.workplace@gmail.com>
2025-07-16 18:26:15 +0530

    added file - components/InteractiveGraph/controls/GeneSearch.vue

---
+++ components/InteractiveGraph/controls/GeneSearch.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="mb-2 flex relative" v-if="geneSearch && selectedColorOption == 'Gene'">
+		<AutoComplete
+			dropdown
+			class="w-full"
+			:model-value="modelValue"
+			@update:model-value="$emit('update:modelValue', $event)"
+			@complete="searchGene"
+			:suggestions="suggestions"
+			@item-select="searchGeneExpression"
+			placeholder="Type gene name to search ..."
+		/>
+
+		<button class="ml-2" v-if="modelValue" @click="clearGeneSelection">
+			<Icon name="akar-icons:circle-x" class="w-5 h-5 text-red-500" />
+		</button>
+	</div>
+</template>
+
+<script setup>
+import { useGeneAPI } from '@/api/geneAPI'
+
+const props = defineProps({
+	geneSearch: { type: Boolean, default: true },
+	selectedColorOption: { type: String, default: null },
+	modelValue: { type: String, default: null },
+})
+
+const suggestions = ref([])
+
+const searchGene = async (event) => {
+	const { getGeneAutocomplete } = useGeneAPI()
+	try {
+		suggestions.value = (await getGeneAutocomplete(event?.query)) || []
+	} catch (err) {
+		console.error('Error fetching gene autocomplete:', err)
+		suggestions.value = []
+	}
+}
+
+const searchGeneExpression = (event) => {
+	emit('gene-selected', event.value)
+}
+
+const clearGeneSelection = () => {
+	emit('update:modelValue', null)
+	emit('gene-cleared')
+}
+
+const emit = defineEmits(['gene-selected', 'gene-cleared', 'update:modelValue'])
+</script>
