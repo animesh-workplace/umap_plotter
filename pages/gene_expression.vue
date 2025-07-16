d8ba9a2f9f94f620a625f03b5c1d9b2bbefec067
Animesh <animesh.workplace@gmail.com>
2025-07-16 18:26:15 +0530

    updated file - pages/gene_expression.vue

--- pages/gene_expression.vue
+++ pages/gene_expression.vue
@@ -4,7 +4,7 @@
 			<h2 class="text-xl font-semibold mb-1">Gene Expression Visualization</h2>
 		</div>
 		<div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 min-w-full mt-2">
-			<GraphManager
+			<InteractiveGraph
 				class="col-span-1 sm:col-span-2 lg:col-span-2 xl:col-start-3 xl:col-span-2 backdrop-blur rounded-lg"
 			/>
 		</div>
