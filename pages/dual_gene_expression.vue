fdb46196dd61af04e213eab789affca5268df72d
Animesh <animesh.workplace@gmail.com>
2025-08-25 20:10:48 +0530

    updated file - pages/dual_gene_expression.vue

--- pages/dual_gene_expression.vue
+++ pages/dual_gene_expression.vue
@@ -1,7 +1,19 @@
 <template>
 	<div class="p-4 grid grid-cols-1 justify-items-center z-50">
 		<div class="mb-2 text-center">
-			<h2 class="text-xl font-semibold mb-1">Gene Expression Visualization</h2>
+			<div class="p-4 lg:px-16">
+				<div class="text-center ltr:sm:text-left rtl:sm:text-right">
+					<h2 class="text-2xl font-bold text-gray-900 md:text-3xl">
+						Dual Gene Expression Visualization
+					</h2>
+
+					<p class="text-gray-500 mt-4 max-w-3xl">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam
+						sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat
+						quisque ut interdum tincidunt duis.
+					</p>
+				</div>
+			</div>
 		</div>
 		<div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-10 min-w-full mt-2">
 			<InteractiveGraph
