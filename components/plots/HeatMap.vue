c2e1c9e0cc47864488901d8a97f4f48890ff18c6
Animesh <animesh.workplace@gmail.com>
2025-08-29 18:01:56 +0530

    updated file - components/plots/HeatMap.vue

--- components/plots/HeatMap.vue
+++ components/plots/HeatMap.vue
@@ -126,14 +126,12 @@ const chartOption = ref({
 	xAxis: {
 		type: 'category',
 		splitArea: { show: false },
-		axisLabel: { fontSize: 12 },
 		axisLabel: { fontFamily: 'Lexend Deca', fontWeight: 500 },
 		data: Array.from({ length: props.maxX + 1 }, (_, i) => i.toString()),
 	},
 	yAxis: {
 		type: 'category',
 		splitArea: { show: false },
-		axisLabel: { fontSize: 12 },
 		axisLabel: { fontFamily: 'Lexend Deca', fontWeight: 500 },
 		data: Array.from({ length: props.maxY + 1 }, (_, i) => i.toString()),
 	},
