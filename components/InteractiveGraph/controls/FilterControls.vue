9ba6634f88732b0aa30c43f3e72f3c3683fb2880
Animesh <animesh.workplace@gmail.com>
2025-09-01 11:05:51 +0530

    updated file - components/InteractiveGraph/controls/FilterControls.vue

--- components/InteractiveGraph/controls/FilterControls.vue
+++ components/InteractiveGraph/controls/FilterControls.vue
@@ -1,48 +1,54 @@
 <template>
-	<div v-if="!noFilter">
-		<div v-if="selectedFilterOption == 'Source'" class="flex gap-3 items-center mb-4 mt-2 justify-center px-4">
+	<div v-if="!noFilter" class="w-full">
+		<div
+			v-if="selectedFilterOption == 'Source'"
+			class="grid grid-cols-3 gap-3 items-center mb-4 mt-2 justify-center px-4"
+		>
 			<motion.div :index="celltype.index" :while-hover="{ scale: 0.97 }" v-for="celltype in cellTypes">
 				<Tag
 					rounded
 					:value="celltype.name"
-					class="cursor-pointer"
+					class="cursor-pointer w-full"
 					@click="FilterCellType(celltype.index)"
 					:severity="celltype.active ? 'success' : 'danger'"
 				>
 					<template #icon>
 						<Icon
 							v-if="celltype.active"
-							class="w-4 h-4 text-green-500"
+							class="!w-4 !h-4 text-green-500"
 							name="akar-icons:tetragon-fill"
 						/>
-						<Icon v-else class="w-4 h-4 text-rose-500" name="akar-icons:tetragon" />
+						<Icon v-else class="!w-4 !h-4 text-rose-500" name="akar-icons:tetragon" />
 					</template>
 				</Tag>
 			</motion.div>
 		</div>
 
-		<div
+		<!-- <div
 			v-if="selectedFilterOption == 'Cluster'"
 			class="flex gap-2 items-center mb-2 mt-2 justify-center px-4"
-		>
-			<motion.div :while-hover="{ scale: 0.95 }" v-for="(cluster, index) in clusters" :key="index">
-				<Tag
-					rounded
-					class="cursor-pointer"
-					:value="cluster.pill_name"
-					@click="FilterCluster(cluster.index)"
-					:severity="cluster.active ? 'success' : 'danger'"
-				>
-					<template #icon>
-						<Icon
-							v-if="cluster.active"
-							class="w-4 h-4 text-green-500"
-							name="akar-icons:tetragon-fill"
-						/>
-						<Icon v-else class="w-4 h-4 text-rose-500" name="akar-icons:tetragon" />
-					</template>
-				</Tag>
-			</motion.div>
+		> -->
+		<div v-if="selectedFilterOption == 'Cluster'" class="grid grid-cols-3 lg:grid-cols-5 lg:max-w-4xl px-4">
+			<div v-for="(cluster, index) in clusters" :key="index">
+				<motion.div class="p-2" :while-hover="{ scale: 0.95 }">
+					<Tag
+						rounded
+						:value="cluster.pill_name"
+						class="cursor-pointer w-full px-2"
+						@click="FilterCluster(cluster.index)"
+						:severity="cluster.active ? 'success' : 'danger'"
+					>
+						<template #icon>
+							<Icon
+								v-if="cluster.active"
+								class="w-4 h-4 text-green-500"
+								name="akar-icons:tetragon-fill"
+							/>
+							<Icon v-else class="w-4 h-4 text-rose-500" name="akar-icons:tetragon" />
+						</template>
+					</Tag>
+				</motion.div>
+			</div>
 		</div>
 	</div>
 </template>
